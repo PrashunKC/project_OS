bc7994f924fdd7c47de76ea8dac3de3d4f7f5568
PrashunKC <unassigned.56@gmail.com>
2025-12-01 00:37:46 +0545

    modified:   src/kernel/io.c

--- src/kernel/io.c
+++ src/kernel/io.c
@@ -1,6 +1,7 @@
 #include "io.h"
 #include "stdint.h"
 
-void outb(uint16_t port, uint8_t val) {
+void outb(uint16_t port, uint8_t val) 
+{
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
